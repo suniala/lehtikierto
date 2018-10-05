07629d663e5534a47c5f6174cce4b6df0811f2e7
Mikko Suniala <mikko.suniala@iki.fi>
2018-10-05 20:18:46 +0300

    ShareStatusLoc working, needs cleanup and a proxy

--- client/src/main/scala/lehtikierto/client/Routes.scala
+++ client/src/main/scala/lehtikierto/client/Routes.scala
@@ -1,7 +1,7 @@
 package lehtikierto.client
 
 import diode.react.{ReactConnectProps, ReactConnectProxy}
-import japgolly.scalajs.react.Callback
+import japgolly.scalajs.react.{Callback, _}
 import japgolly.scalajs.react.component.Generic
 import japgolly.scalajs.react.extra.router.{BaseUrl, Redirect, Resolution, Router, RouterConfig, RouterConfigDsl, RouterCtl}
 import japgolly.scalajs.react.vdom.html_<^
@@ -10,13 +10,13 @@ import lehtikierto.client.modules.{Dashboard, ShareStatusView, ShareView}
 import lehtikierto.client.services.{RootModel, SPACircuit}
 import lehtikierto.shared.{ShareId, User}
 import org.scalajs.dom.html.Anchor
-import japgolly.scalajs.react._
 
 object Routes {
   sealed trait Loc
   case object DashboardLoc extends Loc
   case object ShareLoc extends Loc
   case class ShareStatusLoc(id: String) extends Loc
+  case class ShareStatusIntLoc(id: Int) extends Loc
   case class ItemLoc(id: Int) extends Loc
 
   sealed trait Page
@@ -29,6 +29,12 @@ object Routes {
     import dsl._
 
     val itemLoc = ScalaComponent.builder[ItemLoc]("Item page")
+      .render(p => <.div(s"Info for item #${p.props.id}"))
+      .build
+    val shareStatusInt = ScalaComponent.builder[ShareStatusIntLoc]("Item page")
+      .render(p => <.div(s"Info for item #${p.props.id}"))
+      .build
+    val shareStatusString = ScalaComponent.builder[ShareStatusLoc]("Item page")
       .render(p => <.div(s"Info for item #${p.props.id}"))
       .build
     val itemLoc2 = ScalaComponent.builder[Loc]("Item page2")
@@ -53,14 +59,20 @@ object Routes {
 //      val d0 = c ~> _
 //      val d = c ~> dynRender(a => itemPage(a))
 //    }
-
+//    val ppp = SPACircuit.wrap((m: RootModel) => m.magazines)
 
     // wrap/connect components to the circuit
     (staticRoute(root, DashboardLoc) ~> renderR(ctl => SPACircuit.wrap((m: RootModel) => m)(proxy => Dashboard(ctl, proxy)))
       | staticRoute("#jaa", ShareLoc) ~> renderR(_ => SPACircuit.wrap((m: RootModel) => m.magazines)(proxy => ShareView(proxy)))
-      //| dynamicRouteCT("#item" / int.caseClass[ItemLoc]) ~> dynRender(a => itemLoc2(a))
-      | dynamicRouteCT("two" / int.caseClass[Module2]) ~> dynRender(m => <.h3(s"Module #2 @ ${m.i}"))
-      //| dynamicRouteCT("#jako" / string.caseClass[ShareStatusLoc]) ~> dynRender(itemPage(_))
+      | dynamicRouteCT("#item" / int.caseClass[ItemLoc]) ~> dynRender(itemLoc(_))
+      | dynamicRouteCT("#item" / int.caseClass[ShareStatusIntLoc]) ~> dynRender(shareStatusInt(_))
+//      | dynamicRouteCT("#jakook" / string("[0-9]+").caseClass[ShareStatusLoc]) ~> dynRender(shareStatusString(_))
+      | dynamicRouteCT("#jako" / string("[0-9]+").caseClass[ShareStatusLoc]) ~> dynRender(s => ShareStatusView(ShareId(s.id)))
+//      | dynamicRouteCT("two" / int.caseClass[Module2]) ~> dynRender(m => <.h3(s"Module #2 @ ${m.i}"))
+//      | dynamicRouteCT("#jako" / string.caseClass[ShareStatusLoc]) ~> dynRender(shareStatusView(_.id))
+//      | dynamicRouteCT("#jako" / string.caseClass[ShareStatusLoc]) ~> dynRender(ShareStatusView(ShareId(_.id)))
+      //      | dynamicRouteCT("#jako" / string.caseClass[ShareStatusLoc]) ~> dynRender(ssl => ShareStatusView(ShareId(ssl.id), ppp))
+//      | dynamicRouteCT("#jako" / string.caseClass[ShareStatusLoc]) ~> dynRender((ssl: ShareStatusLoc) => SPACircuit.wrap((m: RootModel) => m.shares)(proxy => ShareStatusView(ShareId(ssl.id), proxy)))
       ).notFound(redirectToPage(DashboardLoc)(Redirect.Replace))
   }.renderWith(layout)
 
